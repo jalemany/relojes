96db3759a3aaa9b2c5d9bbbd6b967b4f9ca7b9ec
Jordi <jordia4@gmail.com>
2023-05-11 11:52:54 +0200

    preparado para practica de tabla de paises

---
+++ js/paises.js
@@ -0,0 +1,23 @@
+console.log("paises.js");
+
+// URL: https://restcountries.com/v3.1/all
+
+// Vamos a montar una tabla con la siguiente información:
+
+// 1.- Nombre del pais
+// 2.- Capital
+// 3.- Población
+// 4.- Mostrar la bandera (opcional)
+// 5.- Mostrar la lista de paises con los que hace frontera (opcional)
+
+/*
+
+    Cuando obtenemos los datos pasamos dichos datos a otra función para pintar la tabla.
+
+    let paises = JSON.parse(xhr.responseText);
+
+    populateData(paises);
+
+
+
+*/
